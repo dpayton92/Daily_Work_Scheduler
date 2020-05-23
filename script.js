f61cdc6265c75f7371ed15f19ea7032b8f814b10
Dpayton <dpayton92@gmail.com>
2020-05-23 02:32:18 -0700

    deleted folder

--- script.js
+++ script.js
@@ -233,26 +233,3 @@ var checkTimes = () => {
   }
 
 }
-
-  var textboxArray = $(".form-control");
-  for (let i = 0; i < textboxArray.length; i++) {
-    let val = getCookie("box" + i);
-    textboxArray[i].value = val != null ? val : "";
-    localStorage.setItem(name, value)
-    localStorage.getItem(name, value)
-  }
-
-  document.getElementById("date").innerHTML = moment();
-
-  console.log(moment())
-  function test(box) {
-    setCookie("box" + box, textboxArray[box].value);
-  }
-
-  function setCookie(name, value) {
-    document.cookie = name + "=" + value + ";path=/;";
-  }
-
-  function getCookie(name) {
-    var v = document.cookie.match("(^|;) ?" + name + "=([^;]*)(;|$)")}   return v ? v[2] : null;
- 
